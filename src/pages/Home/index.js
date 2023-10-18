37ae6baa75e9f6af17fe86c89ef99ab293a9307e
PauloMiguel13000git config --global user.name PauloMiguel13000 <guell.28@hotmail.com>
2023-10-18 17:16:16 -0300

    parte do projeto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,36 @@
+import { useEffect, useState} from 'react';
+import api from '../../services/api';
 
-function Home() {
-    return (
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
-        <div><h1>Bem vindo ao home</h1></div>
-       
-      );
+function Home(){
+  const [filmes, setFilmes] = useState([]);
+
+
+  useEffect(()=>{
+
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params:{
+         api_key: "8107139d33e27b01674cffd5d967e264",
+         language: "pt-BR",
+         page: 1,
+        }
+      })
+
+      console.log(response.data.results);
+
+    }
+
+    loadFilmes();
+
+  }, [])
+
+  return(
+    <div>
+      <h1>BEM VINDO A HOME</h1>
+    </div>
+  )
 }
 
-export default Home ;
+export default Home;
